08e112f185c769043f72fcdf5ff4058164eb07f2
Daniel Jeong <danielyj147@gmail.com>
2025-09-24 17:17:44 -0400

    :construction: lab4 part 2

--- labs/lab04/Lab4.py
+++ labs/lab04/Lab4.py
@@ -1,5 +1,7 @@
 import math
 import doctest
+import nltk
+nltk.download('punkt_tab')
 
 def getVocab(vocab_fname: str) -> set: 
     """
@@ -16,8 +18,11 @@ def getVocab(vocab_fname: str) -> set:
     >>> len(getVocab('data/glove_vocab.txt'))
     400003
     """
-
-    pass
+    a = ['[UNK]','[BOS]','[EOS]']
+    with open(vocab_fname) as f:
+        for line in f:
+            a.append(line)
+    return a
 
 def preprocess(textfname:str, mark_ends: bool) -> list:
     """
@@ -37,8 +42,24 @@ def preprocess(textfname:str, mark_ends: bool) -> list:
     [['one', 'thing', 'was', 'certain', ',', 'that', 'the', '_white_', 'kitten', 'had', 'had', 'nothing', 'to', 'do', 'with', 'it', ':', '—it', 'was', 'the', 'black', 'kitten', '’', 's', 'fault', 'entirely', '.'], ['for', 'the', 'white', 'kitten', 'had', 'been', 'having', 'its', 'face', 'washed', 'by', 'the', 'old', 'cat', 'for', 'the', 'last', 'quarter', 'of', 'an', 'hour', '(', 'and', 'bearing', 'it', 'pretty', 'well', ',', 'considering', ')', ';', 'so', 'you', 'see', 'that', 'it', '_couldn', '’', 't_', 'have', 'had', 'any', 'hand', 'in', 'the', 'mischief', '.']]
 
     """
-    pass
+    text = ""
+    result = []
+    with open(textfname) as f:
+        for line in f:
+            text += line
+    
+    sents = nltk.sent_tokenize(text)
+    for sent in sents: 
+        a =[]
+        sent = sent.lower()
+        if(mark_ends):
+            a = ['[BOS]', *nltk.word_tokenize(sent), '[EOS]']
+        else:
+            a = nltk.word_tokenize(sent)
 
+        result.append(a)
+    return result
+    
 
 def TestBigramFreqs(freq_dict, print_non1 = False):
     """
@@ -74,41 +95,43 @@ def getBigramFreqs(preprocessed_text:list, vocab:set) -> dict:
     {2: [('kitten', 'had'), ('.', '[EOS]'), ('for', 'the')]}
 
     """
-    pass
+    
+    
+    # return wordcount 
 
-def getBigramProb(bigram: tuple, smooth: str, **kwargs):
-    """
-    Args:
-        bigram: the tuple of the bigram you want the prob of
-        smooth: MLE (no smoothing), add-k where you add k to all bigram counts. Returns -1 if invalid smooth is entered. 
-        **kwargs: other parameters you might want. 
+# def getBigramProb(bigram: tuple, smooth: str, **kwargs):
+#     """
+#     Args:
+#         bigram: the tuple of the bigram you want the prob of
+#         smooth: MLE (no smoothing), add-k where you add k to all bigram counts. Returns -1 if invalid smooth is entered. 
+#         **kwargs: other parameters you might want. 
 
-        Hint: think about what parameters do you want to pass in so you minimize redundant computation. 
+#         Hint: think about what parameters do you want to pass in so you minimize redundant computation. 
 
-    Returns:
-        float with prob. 
-        Return -1.0 if invalid smoothing value is entered. 
+#     Returns:
+#         float with prob. 
+#         Return -1.0 if invalid smoothing value is entered. 
 
-        Here are the probabilities for some bigrams from data/test.txt
+#         Here are the probabilities for some bigrams from data/test.txt
         
-        MLE: 
-            ('one', 'thing') 1.0
-            ('kitten', 'had') 0.6666666666666666
-            ('cat', 'had') 0.0
-            ('had', 'had') 0.25
-            ('on', 'the') 0.0
-            ('held', 'a') 0.0
-            ('zzzzzzz', 'the') 0.0
+#         MLE: 
+#             ('one', 'thing') 1.0
+#             ('kitten', 'had') 0.6666666666666666
+#             ('cat', 'had') 0.0
+#             ('had', 'had') 0.25
+#             ('on', 'the') 0.0
+#             ('held', 'a') 0.0
+#             ('zzzzzzz', 'the') 0.0
 
-        add-1:
-            ('one', 'thing') 4.999950000499995e-06
-            ('kitten', 'had') 7.499887501687475e-06
-            ('cat', 'had') 2.4999750002499977e-06
-            ('had', 'had') 4.999912501531223e-06
-            ('on', 'the') 2.499981250140624e-06
-            ('held', 'a') 2.499981250140624e-06
-            ('zzzzzzz', 'the') 2.4999562507656116e-06
+#         add-1:
+#             ('one', 'thing') 4.999950000499995e-06
+#             ('kitten', 'had') 7.499887501687475e-06
+#             ('cat', 'had') 2.4999750002499977e-06
+#             ('had', 'had') 4.999912501531223e-06
+#             ('on', 'the') 2.499981250140624e-06
+#             ('held', 'a') 2.499981250140624e-06
+#             ('zzzzzzz', 'the') 2.4999562507656116e-06
 
-    """
-    pass
+#     """
+#     pass
 
